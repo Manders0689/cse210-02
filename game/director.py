1273cdc4f0e404645ed70c0f1f4fae7440d1ae7e
Manders0689 <Mandy.beck05@gmail.com>
2022-05-06 08:23:30 -0600

    Try again

---
+++ game/director.py
@@ -0,0 +1,15 @@
+# object: Card
+# Responsibility: To choose card and hold value.
+#
+# Behavior:
+#   -pick a card
+#
+# State:
+#   -card value
+
+# class: Card
+# Attributes:
+#   -value: integer
+#
+# Method:
+#   -pick_card(): None
