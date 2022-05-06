674f4d1b9a23e1bc53423f3e678b7ad65de2a77a
Manders0689 <Mandy.beck05@gmail.com>
2022-05-06 08:29:05 -0600

    Updated Card and Director

--- game/director.py
+++ game/director.py
@@ -1,15 +1,33 @@
-# object: Card
-# Responsibility: To choose card and hold value.
+# object: Director
+# Responsibility: controls the action in the game of picking and evaluating cards and their score.
 #
-# Behavior:
-#   -pick a card
+# Behaviors:
+#   -start Hilo game
+#   -guess whether card is higher or lower from previous card
+#   -picks card and calculates score
+#   -shows card and score
+#   -gets answer to play again
 #
 # State:
-#   -card value
+#   -first card
+#   -second card
+#   -user guess of high or low
+#   -score
+#   -total score
+#   -user input for whether they will continue playing
 
-# class: Card
+# class: Director
 # Attributes:
-#   -value: integer
+#   -card1: integer<Card>
+#   -card2: integer<Card>
+#   -hi_lo: boolean or string
+#   -score: integer
+#   -total_score: integer
+#   -is_playing: boolean
 #
-# Method:
-#   -pick_card(): None
+# Methods:
+#   -start_game(): None
+#   -guess_hl(): None
+#   -do_updates(): None
+#   -do_outputs(): None
+#   -get_inputs(): None
